6160f06599418f72b145454a35ec947c630e8887
Mitvi Dattani <mitvidattani@gmail.com>
2026-06-12 00:43:19 +0530

    Displaying dynamic stock tables

--- CAPM_Return.py
+++ CAPM_Return.py
@@ -32,7 +32,6 @@ SP500.columns = ['sp500']
 
 stocks_df = pd.DataFrame() # Create an empty DataFrame to store stock data
 
-
 for stock in stocks_list:
     data = yf.download(stock, period=f"{year}y")
     print(f"\n===== DATA FOR {stock} =====")
@@ -44,11 +43,22 @@ for stock in stocks_list:
 
 stocks_df.reset_index(inplace=True) # Reset the index of the DataFrame to make 'Date' a regular column instead of an index
 SP500.reset_index(inplace=True) # Reset the index of the S&P 500 DataFrame to make 'Date' a regular column instead of an index
-SP500.columns = ['Date', 'sp500'] # Rename the columns of the S&P 500 DataFrame to 'Date' and 'sp500' for clarity and consistency with the stocks DataFrame
+SP500.columns = ['Date', 'sp500'] 
+# Rename the columns of the S&P 500 DataFrame to 'Date' and 'sp500' for clarity and consistency with the stocks DataFrame
 
 stocks_df['Date'] = stocks_df['Date'].astype("datetime64[ns]") # Convert the 'Date' column to datetime format
 stocks_df['Date'] = stocks_df['Date'].apply(lambda x:str(x)[:10]) # Format the 'Date' column to keep only the date part (YYYY-MM-DD)
 stocks_df['Date'] = pd.to_datetime(stocks_df['Date']) # Convert the 'Date' column back to datetime format after formatting
-stocks_df = pd.merge(stocks_df, SP500, on='Date', how='inner') # Merge the stocks DataFrame with the S&P 500 DataFrame on the 'Date' column using an inner join to keep only matching dates 
+stocks_df = pd.merge(stocks_df, SP500, on='Date', how='inner') 
+# Merge the stocks DataFrame with the S&P 500 DataFrame on the 'Date' column using an inner join to keep only matching dates 
 
 print(stocks_df)
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.markdown("### Dataframe head")
+    st.dataframe(stocks_df.head(), use_container_width=True) # Display the first few rows of the merged DataFrame in the Streamlit app
+with col2:
+    st.markdown("### Dataframe tail")
+    st.dataframe(stocks_df.tail(), use_container_width=True) # Display the last few rows of the merged DataFrame in the Streamlit app    
+    
